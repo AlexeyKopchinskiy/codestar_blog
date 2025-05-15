6517553dbafbb3baa911bd3c1ca28ca95aca1ec1
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-05-15 16:38:21 +0200

    Finish setting up Djungo environment

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from blog.views import my_blog
+from blog.views import blog
 
 urlpatterns = [
-    path('blog/', my_blog, name='blog'),
+    path('blog/', blog, name='blog'),
     path('admin/', admin.site.urls),
 ]
